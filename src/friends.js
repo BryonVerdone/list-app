27af0e632ec827825d591062123e27b3c425cf54
BryonVerdone <bryonverdone@gmail.com>
2022-04-24 21:12:47 -0400

    init

---
+++ src/friends.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 1,
+    img: 'https://storage.googleapis.com/production-bluehost-v1-0-0/090/1216090/iRr7Pljj/38ff8d53885a48f081d8c37f369a54a1',
+    name: 'Jeremy',
+    job: 'Musician',
+  },
+  {
+    id: 2,
+    img: 'https://storage.googleapis.com/production-bluehost-v1-0-0/090/1216090/iRr7Pljj/6662228117a945cd93561f365d1a6941',
+    name: 'Earl',
+    position: 'Vocals/Guitar',
+  },
+  {
+    id: 3,
+    img: 'https://storage.googleapis.com/production-bluehost-v1-0-0/090/1216090/iRr7Pljj/ab7ed94585d744d684eff658f5e1230e',
+    name: 'Ben',
+    position: 'Drums',
+  },
+  {
+    id: 4,
+    img: 'https://storage.googleapis.com/production-bluehost-v1-0-0/090/1216090/iRr7Pljj/058525fcf8ba41af93365f4933337478',
+    name: 'Dave',
+    position: 'Keys/Syth',
+  },
+  {
+    id: 5,
+
+    img: 'https://storage.googleapis.com/production-bluehost-v1-0-0/090/1216090/iRr7Pljj/e7e15f6bdc2a492e92a815d4fff0f295',
+    name: 'Bryon',
+    position: 'Bass',
+  },
+];
